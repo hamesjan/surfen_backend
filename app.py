491a2015f5ab9f707e4468e5276c06ba7636b0b6
hamesjan <jhan31903@gmail.com>
2023-07-16 20:19:33 -0700

    changed port

--- app.py
+++ app.py
@@ -121,5 +121,9 @@ def optical_process(video_bytes):
     return public_url
 
 
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=port)
+
+
